3a57b6de5b475267b98f48f2f0eaebe084f3031d
Arjun <arjunbhushan00@gmail.com>
2022-03-04 16:49:14 +0000

    3b
    This commit marks the4 end of 3b where we display the note on the OLED screen and

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,11 @@
   const int HKOW_BIT = 5;
   const int HKOE_BIT = 6;
 
+  uint8_t keyArray[7];
+  const int32_t stepSizes [] = {51076056, 54113197, 57330935, 60740010, 64351798, 68178356, 72232452, 76527617, 81078186, 85899345, 91007186, 96418755};
+  const char* notes[13] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", " "};
+  volatile int32_t currentStepSize;
+
 //Display driver object
 U8G2_SSD1305_128X32_NONAME_F_HW_I2C u8g2(U8G2_R0);
 
@@ -121,31 +126,90 @@ void setRow(uint8_t rowIdx){
 
 void loop() {
   // put your main code here, to run repeatedly:
+  uint8_t key_pressed_step_size = 12;
 
-  uint8_t keyArray[7];
   static uint32_t next = millis();
   static uint32_t count = 0;
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  
 
-  for (int i = 0; i < 3; i++) {
+  for(uint8_t i = 0; i < 3; i++){
     setRow(i);
     delayMicroseconds(3);
-    uint8_t keys = readCols();
-    keyArray[i] == keys;
+    keyArray[i] = readCols();
   }
 
 
-  u8g2.setCursor(2,10);
-  u8g2.print(keyArray[0],HEX);
+  u8g2.clearBuffer();         // clear the internal memory
+  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font u8g2_font_ncenB08_tr
+  u8g2.drawStr(2,10,"hello world");  // write something to the internal memory
   u8g2.setCursor(2,20);
+  u8g2.print(keyArray[0],HEX);
   u8g2.print(keyArray[1],HEX);
-  u8g2.setCursor(2,30);
   u8g2.print(keyArray[2],HEX);
- 
-  u8g2.sendBuffer();      
 
+  switch(keyArray[0]){
+    case 0xE:
+      key_pressed_step_size= 0;
+ 
+      break;
+    case 0xD:
+      key_pressed_step_size= 1;
+
+      break;
+    case 0xB:
+      key_pressed_step_size= 2;
+  
+      break;     
+    case 0x7:
+      key_pressed_step_size = 3;
+      break;
+    default:
+      break;    
+  }
+
+  switch(keyArray[1]){
+    case 0xE:
+      key_pressed_step_size = 4;
+      
+      break;
+    case 0xD:
+      key_pressed_step_size = 5;
+      
+      break;
+    case 0xB:
+      key_pressed_step_size = 6;
+      
+      break;     
+    case 0x7:
+      key_pressed_step_size = 7;
+      
+      break;
+    default:
+      break;    
+  }
+
+  switch(keyArray[2]){
+    case 0xE:
+      key_pressed_step_size = 8;
+      
+      break;
+    case 0xD:
+      key_pressed_step_size = 9;
+      
+      break;
+    case 0xB:
+      key_pressed_step_size = 10;
+     
+      break;     
+    case 0x7:
+      key_pressed_step_size = 11;
+      break;
+    default:
+      break;    
+  }
+
+  u8g2.setCursor(2,30);
+  u8g2.print(notes[key_pressed_step_size]);
+  u8g2.sendBuffer();    
   
 }
 
