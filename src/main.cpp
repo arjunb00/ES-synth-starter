d70a2e2185350f2d13cb4e8cdc48df513ca09d9a
Arjun <arjunbhushan00@gmail.com>
2022-03-04 18:53:54 +0000

    CW2 Lab1
    Finished CW2 Lab1

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <iostream>
+#include <STM32FreeRTOS.h> 
 
 //Constants
   const uint32_t interval = 100; //Display update interval
@@ -33,7 +34,8 @@
   const int HKOW_BIT = 5;
   const int HKOE_BIT = 6;
 
-  uint8_t keyArray[7];
+  volatile uint8_t keyArray[7];
+  volatile uint8_t key_pressed_step_size = 12;
   const int32_t stepSizes [] = {51076056, 54113197, 57330935, 60740010, 64351798, 68178356, 72232452, 76527617, 81078186, 85899345, 91007186, 96418755};
   const char* notes[13] = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", " "};
   volatile int32_t currentStepSize;
@@ -53,36 +55,11 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-
-  //Set pin directions
-  pinMode(RA0_PIN, OUTPUT);
-  pinMode(RA1_PIN, OUTPUT);
-  pinMode(RA2_PIN, OUTPUT);
-  pinMode(REN_PIN, OUTPUT);
-  pinMode(OUT_PIN, OUTPUT);
-  pinMode(OUTL_PIN, OUTPUT);
-  pinMode(OUTR_PIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(C0_PIN, INPUT);
-  pinMode(C1_PIN, INPUT);
-  pinMode(C2_PIN, INPUT);
-  pinMode(C3_PIN, INPUT);
-  pinMode(JOYX_PIN, INPUT);
-  pinMode(JOYY_PIN, INPUT);
-
-  //Initialise display
-  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
-  delayMicroseconds(2);
-  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
-  u8g2.begin();
-  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
-
-  //Initialise UART
-  Serial.begin(9600);
-  Serial.println("Hello World");
+void sampleISR(){
+  static uint32_t phaseAcc = 0; 
+  phaseAcc += currentStepSize;
+  uint32_t Vout = phaseAcc >> 24; 
+  analogWrite(OUTR_PIN, Vout + 128);
 }
 
 uint8_t readCols(){
@@ -124,92 +101,183 @@ void setRow(uint8_t rowIdx){
 
 }
 
+
+void scanKeysTask(void * pvParameters) {
+
+  const TickType_t xFrequency = 50/portTICK_PERIOD_MS; 
+  TickType_t xLastWakeTime = xTaskGetTickCount(); 
+
+  while(1){
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    uint8_t key_Pressed = 0;
+
+    for(uint8_t i = 0; i < 3; i++){
+      setRow(i);
+      delayMicroseconds(3);
+      keyArray[i] = readCols();
+    }
+
+    switch(keyArray[0]){
+      case 0xE:
+        key_pressed_step_size= 0;
+        key_Pressed = 1;
+        break;
+      case 0xD:
+        key_pressed_step_size= 1;
+        key_Pressed = 1;
+        break;
+      case 0xB:
+        key_pressed_step_size= 2;
+        key_Pressed = 1;
+        break;     
+      case 0x7:
+        key_pressed_step_size = 3;
+        key_Pressed = 1;
+        break;
+      default:
+        break;    
+    }
+
+    switch(keyArray[1]){
+      case 0xE:
+        key_pressed_step_size = 4;
+        key_Pressed = 1;
+        break;
+      case 0xD:
+        key_pressed_step_size = 5;
+        key_Pressed = 1;
+        break;
+      case 0xB:
+        key_pressed_step_size = 6;
+        key_Pressed = 1;
+        break;     
+      case 0x7:
+        key_pressed_step_size = 7;
+        key_Pressed = 1;
+        break;
+      default:
+        break;    
+    }
+
+    switch(keyArray[2]){
+      case 0xE:
+        key_pressed_step_size = 8;
+        key_Pressed = 1;
+        break;
+      case 0xD:
+        key_pressed_step_size = 9;
+        key_Pressed = 1;
+        break;
+      case 0xB:
+        key_pressed_step_size = 10;
+        key_Pressed = 1;
+        break;     
+      case 0x7:
+        key_pressed_step_size = 11;
+        key_Pressed = 1;
+        break;
+      default:
+        break;    
+    }
+    
+    if(key_Pressed == 0){
+      key_pressed_step_size = 12;
+      __atomic_store_n(&currentStepSize, 0, __ATOMIC_RELAXED);
+    }
+    else{
+      __atomic_store_n(&currentStepSize, stepSizes[key_pressed_step_size], __ATOMIC_RELAXED);
+    }
+  }
+}
+
+void displayUpdateTask(void * pvParameters){
+
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS; 
+  TickType_t xLastWakeTime = xTaskGetTickCount(); 
+
+  while(1){
+    u8g2.clearBuffer();         // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font u8g2_font_ncenB08_tr
+    u8g2.drawStr(2,10,"hello world");  // write something to the internal memory
+    u8g2.setCursor(2,20);
+    u8g2.print(keyArray[0],HEX);
+    u8g2.print(keyArray[1],HEX);
+    u8g2.print(keyArray[2],HEX);
+    u8g2.setCursor(2,30);
+    u8g2.print(notes[key_pressed_step_size]);
+    u8g2.sendBuffer();
+  }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  //Set pin directions
+  pinMode(RA0_PIN, OUTPUT);
+  pinMode(RA1_PIN, OUTPUT);
+  pinMode(RA2_PIN, OUTPUT);
+  pinMode(REN_PIN, OUTPUT);
+  pinMode(OUT_PIN, OUTPUT);
+  pinMode(OUTL_PIN, OUTPUT);
+  pinMode(OUTR_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(C0_PIN, INPUT);
+  pinMode(C1_PIN, INPUT);
+  pinMode(C2_PIN, INPUT);
+  pinMode(C3_PIN, INPUT);
+  pinMode(JOYX_PIN, INPUT);
+  pinMode(JOYY_PIN, INPUT);
+
+  //Initialise display
+  setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
+  delayMicroseconds(2);
+  setOutMuxBit(DRST_BIT, HIGH);  //Release display logic reset
+  u8g2.begin();
+  setOutMuxBit(DEN_BIT, HIGH);  //Enable display power supply
+
+  TIM_TypeDef *Instance = TIM1; 
+  HardwareTimer *sampleTimer = new HardwareTimer(Instance);
+
+  sampleTimer->setOverflow(22000, HERTZ_FORMAT); 
+  sampleTimer->attachInterrupt(sampleISR); 
+  sampleTimer->resume();
+
+  TaskHandle_t scanKeysHandle = NULL; 
+  TaskHandle_t displayUpdateHandle = NULL; 
+  xTaskCreate( 
+    scanKeysTask,  /* Function that implements the task */ 
+    "scanKeys",   /* Text name for the task */ 
+    64,        /* Stack size in words, not bytes */ 
+    NULL,    /* Parameter passed into the task */ 
+    1,   /* Task priority */ 
+    &scanKeysHandle );  /* Pointer to store the task handle */
+  
+  xTaskCreate( 
+    displayUpdateTask,  /* Function that implements the task */ 
+    "displayKeys",   /* Text name for the task */ 
+    256,        /* Stack size in words, not bytes */ 
+    NULL,    /* Parameter passed into the task */ 
+    1,   /* Task priority */ 
+    &displayUpdateHandle );  /* Pointer to store the task handle */
+
+
+  //Initialise UART
+  Serial.begin(9600);
+  Serial.println("Hello World");
+
+  vTaskStartScheduler();
+}
+
+
+
+
+
+
 void loop() {
   // put your main code here, to run repeatedly:
-  uint8_t key_pressed_step_size = 12;
 
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-
-  for(uint8_t i = 0; i < 3; i++){
-    setRow(i);
-    delayMicroseconds(3);
-    keyArray[i] = readCols();
-  }
-
-
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font u8g2_font_ncenB08_tr
-  u8g2.drawStr(2,10,"hello world");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(keyArray[0],HEX);
-  u8g2.print(keyArray[1],HEX);
-  u8g2.print(keyArray[2],HEX);
-
-  switch(keyArray[0]){
-    case 0xE:
-      key_pressed_step_size= 0;
- 
-      break;
-    case 0xD:
-      key_pressed_step_size= 1;
-
-      break;
-    case 0xB:
-      key_pressed_step_size= 2;
-  
-      break;     
-    case 0x7:
-      key_pressed_step_size = 3;
-      break;
-    default:
-      break;    
-  }
-
-  switch(keyArray[1]){
-    case 0xE:
-      key_pressed_step_size = 4;
-      
-      break;
-    case 0xD:
-      key_pressed_step_size = 5;
-      
-      break;
-    case 0xB:
-      key_pressed_step_size = 6;
-      
-      break;     
-    case 0x7:
-      key_pressed_step_size = 7;
-      
-      break;
-    default:
-      break;    
-  }
-
-  switch(keyArray[2]){
-    case 0xE:
-      key_pressed_step_size = 8;
-      
-      break;
-    case 0xD:
-      key_pressed_step_size = 9;
-      
-      break;
-    case 0xB:
-      key_pressed_step_size = 10;
-     
-      break;     
-    case 0x7:
-      key_pressed_step_size = 11;
-      break;
-    default:
-      break;    
-  }
-
-  u8g2.setCursor(2,30);
-  u8g2.print(notes[key_pressed_step_size]);
-  u8g2.sendBuffer();    
+    
   
 }
 
